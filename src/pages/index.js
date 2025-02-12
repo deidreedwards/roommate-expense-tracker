e72f52c9bc9e9530b80dd9b7262deead12d8e921
deidreedwards <47465257+deidreedwards@users.noreply.github.com>
2025-02-12 08:26:09 -0500

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import RoommateExpenseTracker from '../components/RoommateExpenseTracker'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <RoommateExpenseTracker />
+    </main>
+  )
+}
